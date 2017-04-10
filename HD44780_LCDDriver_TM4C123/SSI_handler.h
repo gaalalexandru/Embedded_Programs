6e97d2951e043b3fde56b24c54864e06126b6898
INTERN\alexandru.gaal <alexandru.gaal-ext@continental-corporation.com>
2017-04-10 17:04:23 +0300

    initial project for HD44780 LCDDriver

---
+++ HD44780_LCDDriver_TM4C123/SSI_handler.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+void SSI0_DataOut(uint8_t data);
+void SSI0_Init(void);
---
+++ HD44780_LCDDriver_TM4C123/ssi_handler.d
@@ -0,0 +1,4 @@
+.\ssi_handler.o: SSI_handler.c
+.\ssi_handler.o: F:\0_Tools\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\ssi_handler.o: SSI_handler.h
+.\ssi_handler.o: ..\..\inc\tm4c123gh6pm.h
