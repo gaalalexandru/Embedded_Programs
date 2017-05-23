82edf6cff2c57c8512d47937afd293bd7cd85bd7
Gaal Alexandru <gsx_16@yahoo.com>
2017-05-23 22:40:37 +0300

    asdada

--- SIM900_SMSReceiveTest_TM4C123_WIP_HW_Fifo/Objects/fifo.d
+++
@@ -1,4 +0,0 @@
-.\objects\fifo.o: fifo.c
-.\objects\fifo.o: F:\0_Tools\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\fifo.o: FIFO.h
-.\objects\fifo.o: F:\0_Tools\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
--- SIM900_SMSReceiveTest_TM4C123_WIP_HW_Fifo_Simplified - Copy/Objects/fifo_simple.d
+++
@@ -1,4 +0,0 @@
-.\objects\fifo_simple.o: fifo_simple.c
-.\objects\fifo_simple.o: fifo_simple.h
-.\objects\fifo_simple.o: F:\0_Tools\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\fifo_simple.o: F:\0_Tools\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
